a1eab82da25e8f2097b167dd974927c9cfca56d1
vinokradina <106469305+vinokradina@users.noreply.github.com>
2024-10-11 10:30:43 +0300

    Add files via upload

---
+++ Cursor/Cursor/Cursor.cpp
@@ -0,0 +1,26 @@
+﻿#include <windows.h> 
+#include <iostream> 
+using namespace std;
+int main()
+{
+	char c;
+	HANDLE hStdOut;
+	COORD coord; 
+	
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "Input new cursor position." << endl;
+	cout << "X = ";
+	cin >> coord.X;
+	cout << "Y = ";
+	cin >> coord.Y;
+	
+	if (!SetConsoleCursorPosition(hStdOut, coord))
+	{
+		cout << "Set cursor position failed." << endl;
+		return GetLastError();
+	}
+	cout << "This is a new position." << endl;
+	cout << "Input any char to exit: ";
+	cin >> c;
+	return 0;
+}
